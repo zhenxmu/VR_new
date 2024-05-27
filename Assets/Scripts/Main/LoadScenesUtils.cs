06eb1b87ef176e0a746553040ab5a912e89e7ee3
zhenxmu <133001909+zhenxmu@users.noreply.github.com>
2024-05-27 22:31:30 +0800

    场景过度加载

--- Assets/Scripts/Main/LoadScenesUtils.cs
+++ Assets/Scripts/Main/LoadScenesUtils.cs
@@ -14,20 +14,29 @@ public class LoadScenesUtils : MonoBehaviour {
     public static Vector3 originalRotationMain = new Vector3(0, 0, 0);
     public static string mainSceneName = "VRmain";
 
+    // 下一个加载的场景
+    public static string nextSceneName;
+
     // 根据碰撞物体名跳转到指定场景
     public static void LoadSceneIfByObjNameOrElseNone(string objName){
         string sceneName = objName.Substring(6);
         if (sceneName.StartsWith("Scene")) {
-            SceneManager.LoadScene(sceneName);
+            LoadScene(sceneName);
+
             // 跳转到其他场景标记已经离开门, 因为其他场景的是自己摆的
             isOnDoor = false;
             Debug.Log("跳转到场景："+sceneName);
         } else if (sceneName.StartsWith("Main")) {
-            SceneManager.LoadScene(mainSceneName);
+            LoadScene(mainSceneName);
             Debug.Log("跳转到主场景");
         } else {
             Debug.Log("未定义的场景跳转名");
         }
+    }
+
+    private static void LoadScene(string sceneName) {
+        SceneManager.LoadScene("IntermediateScene");
+        nextSceneName = sceneName;
     }
 
     // 保存主场景中的位置信息到静态类
