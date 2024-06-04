ad86746892e6d3b35ae254a1ad3a9c118e239807
zhenxmu <133001909+zhenxmu@users.noreply.github.com>
2024-06-04 10:57:35 +0800

    华容道反馈修改

---
+++ Assets/Scripts/Huarongdao/newControls.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class MaterialPropertiesController : MonoBehaviour
+{
+    private Renderer objectRenderer;
+
+    void Start()
+    {
+        objectRenderer = GetComponent<Renderer>();
+        if (objectRenderer == null)
+        {
+            Debug.LogError("Renderer component not found on the object!");
+        }
+    }
+
+    public void SetMetallicToOne()
+    {
+        if (objectRenderer != null)
+        {
+            objectRenderer.material.SetFloat("_Metallic", 1.0f);
+        }
+    }
+
+    public void SetMetallicToZero()
+    {
+        if (objectRenderer != null)
+        {
+            objectRenderer.material.SetFloat("_Metallic", 0.0f);
+        }
+    }
+}
